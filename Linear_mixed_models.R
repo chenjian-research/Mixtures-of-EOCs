163e3f33fcd8fa8b4c2f6522ddd768fb5f8f7b15
CJ-research <86604455+chenjian-research@users.noreply.github.com>
2022-09-20 23:01:36 +0800

    Update Linear_mixed_models.R

--- Linear_mixed_models.R
+++ Linear_mixed_models.R
@@ -1,3 +1,4 @@
+# R code
 library(lme4)
 library(Matrix)
 library(lmerTest)
@@ -5,19 +6,19 @@ library(MuMIn)
 data<-read.csv("Linear_mixed_models.csv", header = T) 
 str(data)
 summary(data)
-fit1<-lmer(celldivision~EOCnumber+overlap+(1|regime)+(1|algae)+(1|light)+(1|concentration)+(1|temperature)+(1|replicate), REML=T, data)
-#celldivision is the growth rates of two algae
+fit1<-lmer(growthrate~CECnumber+overlap+(1|regime)+(1|algae)+(1|light)+(1|concentration)+(1|temperature)+(1|replicate), REML=T, data)
+#growthrate is the growth rates of two algae
 summary(fit1)
 anova(fit1)
 ranova(fit1)
 r.squaredLR(fit1)
-# effect size of EOC number
-c=data$EOCnumber
+# effect size of CEC number
+c=data$CECnumber
 b =fixef(fit1)[2]  
 se=0.0001787
-var(c)*(b^2 - se^2)/var(data$celldivision)*100
+Effect1<-var(c)*(b^2 - se^2)/var(data$growthrate)*100
 #effect size of overlap
 c=data$overlap
 b =fixef(fit1)[3]  
 se= 8.646e-05
-var(c)*(b^2 - se^2)/var(data$celldivision)*100
+Effect2<-var(c)*(b^2 - se^2)/var(data$growthrate)*100
